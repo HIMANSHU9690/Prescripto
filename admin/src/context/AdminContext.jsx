0a43975da858ac5a53dedde7b278919ed4e06b31
HK <hkhimanshu3104@gmail.com>
2024-11-16 17:12:17 +0530

    Update AdminContext.jsx

--- admin/src/context/AdminContext.jsx
+++ admin/src/context/AdminContext.jsx
@@ -6,7 +6,7 @@ export const AdminContext = createContext();
 
 const AdminContextProvider = (props) => {
   const [aToken, setAToken] = useState(localStorage.getItem('aToken')?localStorage.getItem('aToken'):'')
-  const backendUrl=import.meta.env.VITE_BACKEND_URL
+  const backendUrl="https://prescripto-backend-bhsg.onrender.com"
   const [doctors,setDoctors]=useState([])
   const[appointments,setAppointments]= useState([])
   const [dashData,setDashData] =useState(false)
