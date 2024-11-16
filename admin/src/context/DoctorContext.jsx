090c43b7d812214e2d4861978bd3cdaa5eb08559
HK <hkhimanshu3104@gmail.com>
2024-11-16 17:12:45 +0530

    Update DoctorContext.jsx

--- admin/src/context/DoctorContext.jsx
+++ admin/src/context/DoctorContext.jsx
@@ -8,7 +8,7 @@ export const DoctorContext=createContext()
 const DoctorContextProvider=(props)=>{
 
 
-   const backendUrl= import.meta.env.VITE_BACKEND_URL
+   const backendUrl= "https://prescripto-backend-bhsg.onrender.com"
    const [dToken,setDToken]=useState((localStorage.getItem('dToken')?localStorage.getItem('dToken'):''))
 
    const[appointments,setAppointments]=useState([])
